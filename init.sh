b33891a96e2fbab06a5c867b7c315ace578d69f1
Min Latt <minminlaxz@gmail.com>
2021-05-26 14:47:06 +0630

    Setting currnet docker container.

---
+++ init.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+echo "source $HOME/.dockerenv" >> $HOME/.zshrc
---
+++ laxz-dk.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+HEAD='\e[7;36m'
+RESET='\e[m'
+OUTPUT='\e[32m'
+NL='\n'
+ERROR='\e[3;31m'
+WARN='\e[3;33m'
+
+function oneLineOutput() {
+    line=$1
+    echo -e "laxz-dk : ${OUTPUT}$line${RESET}"
+}
+
+function descriptionOutput() {
+    line=$1
+    echo -e "${WARN}laxz-dk : $line ${RESET}"
+}
+
+function warningOutput() {
+    line=$1
+    echo -e "${ERROR}laxz-dk : $line ${RESET}"
+}
+
+function hasDocker() {
+    dpkg-query -l docker >/dev/null 2>&1
+    return $?
+}
+
+
+hasDocker
+if [[ $? -eq 0 ]]; then
+    descriptionOutput "Docker is installed."
+else
+    warningOutput "cannot be found docker!"
+fi
+
+docker ps -a --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Image}}"
+
+echo -e "${WARN}"
+read -p "Enter Conatiner Name: " containerName
+
+echo -e "${OUTPUT}"
+docker ps -aqf "name=^$containerName" --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Image}}"
+echo -e "${RESET}"
+
+containerID=$(docker ps -aqf "name=^$containerName")
+scriptDir=$(dirname "$0")
+
+# sed -i 's/CURRENT_CONTAINER=/CURRENT_CONTAINER=\$containerID/' ./dockerenv
+echo "export CURRENT_CONTAINER=$containerID" > "$scriptDir/.dockerenv"
+mv $scriptDir/.dockerenv $HOME/.dockerenv
